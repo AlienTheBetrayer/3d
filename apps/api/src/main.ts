4df64f98bb9afee90403bc9632fd348863fdc8bb
alienthebetrayer <gpichkurov@gmail.com>
2026-09-05 01:04:19 +0300

    oxlint + prettier shared config + pipeline

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,9 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module.js";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module.js';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	await app.listen(3002);
-	console.log(`API is running on http://localhost:3001`);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3001);
+  console.log(`API is running on http://localhost:3001`);
 }
 bootstrap();
